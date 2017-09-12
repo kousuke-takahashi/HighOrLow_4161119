22a539b116df9f2abdf9d88f01e38bb7675ce8eb
kousuke-takahashi <kousuke.takahashi.sys@gmail.com>
2017-09-12 15:05:31 +0900

    完成#1

--- src/HorL.java
+++ src/HorL.java
@@ -10,6 +10,47 @@ public class HorL {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("HIGH OR LOW GAME!!!");
+		
+		while(win){
+			mnum = rnd.nextInt(13)+1;
+			cnum = rnd.nextInt(13)+1;
+			if(mnum == cnum) continue;
+			System.out.println("相手の数字→" + cnum);
+			System.out.println(" ");
+			System.out.println("HIGH OR LOW?");
+			System.out.println(" ");
+			System.out.print("HIGHなら1 LOWなら2を入力→");
+			num = sc.nextInt();
+			System.out.println(" ");
+			
+			if(num == 1){
+				System.out.println("COM" + cnum + " YOU" + mnum);
+				if(mnum > cnum){
+					System.out.println("あたり！ 現在のポイント→" + (p = p + mnum));
+					System.out.println(" ");
+				}
+				
+				else{
+					System.out.println("はずれ！ ゲーム終了！最終ポイント→" + p);
+					System.out.println(" ");
+					win = false;
+				}
+			}
+			
+			else if(num == 2){
+				if(mnum < cnum){
+					System.out.println("あたり！ 現在のポイント→" + (p = p + mnum));
+					System.out.println(" ");
+				}
+				
+				else{
+					System.out.println("はずれ！ ゲーム終了！最終ポイント→" + p);
+					System.out.println(" ");
+					win = false;
+				}
+			}
+		}
+
 	}
 
 }
